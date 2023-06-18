64d73fbbe4fbdf22424fbc30139fde6659822556
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-06-18 10:17:59 -0500

    Add new test case for integer arithmetic in the VM

---
+++ vm/vm_test.go
@@ -0,0 +1,82 @@
+package vm
+
+import (
+	"fmt"
+	"parser/Documents/Code/monkey-go/compiler"
+	"parser/Documents/Code/monkey-go/object"
+	"testing"
+
+	"github.com/kitasuke/monkey-go/ast"
+	"github.com/kitasuke/monkey-go/lexer"
+	"github.com/kitasuke/monkey-go/parser"
+)
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		comp := compiler.New()
+		if err := comp.Compile(program); err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New(comp.Bytecode())
+		err := vm.Run()
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.LastPoppedStackElem()
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
+}
+
+func testIntegerObject(expected int64, actual interface{}) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)",
+			actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+	}
+
+	return nil
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2},
+	}
+
+	runVmTests(t, tests)
+}
